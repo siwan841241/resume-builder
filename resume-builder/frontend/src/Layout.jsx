d4f5346f3df7a8b359dbb8483217ccd8d6fd6cf0
Jitendra Kumar <kirankumari47634@gmail.com>
2024-08-24 17:42:09 +0000

    reactroutedom-config

---
+++ resume-builder/frontend/src/Layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+
+
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
--- resume-builder/frontend/src/main.jsx
+++ resume-builder/frontend/src/main.jsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './pages/Home.jsx'
+import SignInPage from './auth/SignInPage.jsx'
+import Dashboard from './pages/Dashboard.jsx'
 
+
+const router = createBrowserRouter ([
+{
+  element:<App />,
+  children:[
+    {
+      path:'/',
+      element:<Home />
+    },
+    {
+      path:'/dashboard',
+      element: <Dashboard />
+    }
+  ]
+},
+{
+  path:'auth/sign-in',
+  element:<SignInPage/>
+}
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+   
   </StrictMode>,
 )

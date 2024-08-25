07d47084efb65492a1bce82d0fb652afdb306e95
Jitendra Kumar <kirankumari47634@gmail.com>
2024-08-25 07:56:55 +0000

    resume_config

--- resume-builder/frontend/src/pages/Dashboard.jsx
+++ resume-builder/frontend/src/pages/Dashboard.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>
-      dashboard
+    <div className='p-10 md:px-20 lg:px-32'>
+      <h2 className='font-bold text-3xl'>My Resume</h2>
+      <p>Start Create AI resume to your next Job role</p>
     </div>
   )
 }
